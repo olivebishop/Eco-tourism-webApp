acde978c0eab523358f130cb207c54994f9c6ced
olivebishop <olivehendrilgen1@gmail.com>
2024-10-27 04:34:07 +0300

    feat: added auth pages

---
+++ app/(auth)/auth/management-portal/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "@/components/signup-form"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center p-4 bg-green-100">
+      <SignupForm />
+    </div>
+  )
+}
