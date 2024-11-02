a30e570872f75bac97224e33fd32b65055fedc97
olivebishop <olivehendrilgen1@gmail.com>
2024-11-02 21:21:10 +0300

    feat: added auth pages for forgot and reset pass

---
+++ app/(auth)/auth/management-portal/reset-password/page.tsx
@@ -0,0 +1,9 @@
+import ResetForm from "@/components/reset-form"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center p-4 bg-green-100">
+      < ResetForm token={""}/>
+    </div>
+  )
+}
