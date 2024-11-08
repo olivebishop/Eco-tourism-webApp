b18228d5cd59aa42c078bb9ba88cd32fd5a6df52
olivebishop <olivehendrilgen1@gmail.com>
2024-11-08 15:59:55 +0300

    fix: chnage in create blogs

--- types/blogs.ts
+++ types/blogs.ts
@@ -1,9 +1,25 @@
-export interface BlogPost {
-    _id: string;
+export interface BlogPost{
+  title: string;
+  author: string;
+  content: string;
+  tags: string[];
+  imageUrl: string;
+}
+
+export interface FormData{
     title: string;
     author: string;
     content: string;
-    tags: string[];
-    imageUrl: string;
-    createdAt: string;
+    tags: string;
   }
+
+
+// export interface BlogPost {
+//     _id: string;
+//     title: string;
+//     author: string;
+//     content: string;
+//     tags: string[];
+//     imageUrl: string;
+//     createdAt: string;
+//   }
